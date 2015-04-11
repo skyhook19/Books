589f261f35032ee29623d8abec36811d21014c84
Anton Glushkov <devdev7@yandex.ru>
2015-04-11 17:31:20 +0300

    Base
    First version

---
+++ Books_01/Models/Authorization/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books_01.Models.Authorization
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? Age { get; set; }
+    }
+}
